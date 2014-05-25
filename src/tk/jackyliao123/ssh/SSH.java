65a3ac9d4ea4d943f75001814fdda9939d1b7444
jackyliao123 <jackyliao2008@gmail.com>
2014-05-24 21:42:03 -0400

    ssh gui

--- src/tk/jackyliao123/ssh/SSH.java
+++ src/tk/jackyliao123/ssh/SSH.java
@@ -1,51 +1,69 @@
 package tk.jackyliao123.ssh;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 
-import com.jcraft.jsch.Channel;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+import com.jcraft.jsch.ChannelShell;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.UserInfo;
 
 public class SSH {
-	public static void main(String args[]) throws Exception{
-		String user = "root";
-		String pass = "Jacky123+VPS";
-		String host = "vps.jackyliao123.tk";
- 
+	static{
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(Exception e){
+		}
+	}
+	public final Session session;
+	public ChannelShell channel;
+	public OutputStream output;
+	public InputStream input;
+	public SSH(String host, int port, String user, String pass) throws JSchException{
 		Properties props = new Properties();
 		props.setProperty("StrictHostKeyChecking", "false");
 		JSch jsch = new JSch();
-		Session session = jsch.getSession(user, host, 22);
+		session = jsch.getSession(user, host, port);
 		session.setUserInfo(new Info());
 		session.setPassword(pass);
+	}
+	public void connectSession() throws JSchException{
 		session.connect();
-		Channel channel = session.openChannel("shell");
+	}
+	public void openChannel() throws JSchException, IOException{
+		channel = (ChannelShell)session.openChannel("shell");
 
-		channel.setInputStream(System.in);
+		input = channel.getInputStream();
 		
-		channel.setOutputStream(System.out);
+		output = channel.getOutputStream();
 		
 		channel.connect(3000);
 	}
-}
-class Info implements UserInfo{
-	public String getPassphrase(){
-		return null;
-	}
-	public String getPassword(){
-		return null;
-	}
-	public boolean promptPassword(String string){
-		return true;
-	}
-	public boolean promptPassphrase(String string){
-		return true;
-	}
-
-	public boolean promptYesNo(String string){
-		return true;
-	}
-	public void showMessage(String string){
+	class Info implements UserInfo{
+		public String getPassphrase(){
+			return null;
+		}
+		public String getPassword(){
+			return null;
+		}
+		public boolean promptPassword(String string){
+			return true;
+		}
+		public boolean promptPassphrase(String string){
+			return true;
+		}
+		public boolean promptYesNo(String message){
+			return JOptionPane.showConfirmDialog(null, message, "jSSH Security Alert", JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION;
+		}
+		public void showMessage(String message){
+			JOptionPane.showMessageDialog(null, message, "jSSH Security Alert", JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
 }
--- src/tk/jackyliao123/ssh/SSHUI.java
+++ src/tk/jackyliao123/ssh/SSHUI.java
@@ -5,20 +5,30 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class SSHUI{
-	public SSHUI(){
+	private SSH ssh;
+	public int cursorX;
+	public int cursorY;
+	public int consoleWidth = 80;
+	public int consoleHeight = 25;
+	public boolean cursor = true;
+	public SSHCanvas canvas;
+	public void showWindow(){
 		Frame frame = new Frame("jSSH");
-		SSHCanvas canvas = new SSHCanvas();
-		
-		canvas.setPreferredSize(new Dimension(640, 300));
-		canvas.setBackground(Color.black);
-		canvas.setCursor(new Cursor(Cursor.TEXT_CURSOR));
-		
+		canvas = new SSHCanvas();
+
 		frame.add(canvas);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
@@ -26,26 +36,119 @@ public class SSHUI{
 		frame.setVisible(true);
 	}
 	public static void main(String[] args){
-		new SSHUI();
+		try{
+			SSHUI sshui = new SSHUI();
+			sshui.ssh = new SSH("vps.lolzballs.cf", 22, "jackyliao123", "hello123");
+			sshui.ssh.connectSession();
+			sshui.ssh.openChannel();
+			sshui.showWindow();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-}
-class SSHCanvas extends Canvas{
-	public int cursorX;
-	public int cursorY;
-	public boolean cursor = true;
-	public void paint(Graphics g){
-		super.paint(g);
-		g.setColor(Color.white);
-		g.setFont(new Font("Courier New", Font.PLAIN, 14));
-		g.drawString("root@vps.jackyliao123.tkTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT", 0, 9);
-		if(cursor)
-			g.fillRect(cursorX, cursorY, 8, 12);
-		else
-			g.drawRect(cursorX, cursorY, 8, 12);
+	StringBuffer b = new StringBuffer();
+	private class InputReader extends Thread{
+		public void run(){
+			try{
+				int i;
+				l:
+				while((i = ssh.input.read()) != -1){
+					if(i == 27){
+						i = ssh.input.read();
+						if(i == -1){
+							break;
+						}
+						else if(i == '['){
+							StringBuffer escape = new StringBuffer();
+							while((i = ssh.input.read()) != -1){
+								if('A' <= i && i <= 'Z' || 'a' <= i && i <= 'z'){
+									i = ssh.input.read();
+									if(i == -1){
+										break l;
+									}
+									break;
+								}
+								else{
+									escape.append((char)i);
+								}
+							}
+							if(i == -1){
+								break;
+							}
+							//System.out.println(escape);
+						}
+					}
+					if(true){
+						if(!(32 <= i && i <= 126 || i == '\r' || i == '\n')){
+							System.out.println(i);
+						}
+						b.append((char)i);
+						String[] lines = b.toString().split("\n");
+						cursorX = lines[lines.length - 1].length();
+						cursorY = lines.length > consoleHeight ? consoleHeight - 1 : (lines.length - 1);
+						canvas.repaint();
+					}
+				}
+				System.exit(0);
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
-}
-class WindowListener extends WindowAdapter{
-	public void windowClosing(WindowEvent e){
-		System.exit(0);
+	private class SSHCanvas extends Canvas{
+		public SSHCanvas(){
+			addKeyListener(new KeyListener());
+			setPreferredSize(new Dimension(640, 300));
+			setBackground(Color.black);
+			setCursor(new Cursor(Cursor.TEXT_CURSOR));
+			new InputReader().start();
+		}
+		public void paint(Graphics g){
+			g.setColor(new Color(0, 255, 0));
+			if(cursor)
+				g.fillRect(cursorX * 8, cursorY * 12, 8, 13);
+			else
+				g.drawRect(cursorX * 8, cursorY * 12, 8, 13);
+			
+			((Graphics2D)g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+			g.setColor(new Color(187, 187, 187));
+			g.setFont(new Font("Courier New", Font.PLAIN, 13));
+			FontMetrics fm = g.getFontMetrics();
+			String[] strings = b.toString().split("\n");
+			for(int i = 0; i < consoleHeight; i ++){
+				String s = "";
+				if(strings.length > consoleHeight){
+					s = strings[strings.length - consoleHeight + i];
+				}
+				else{
+					if(i < strings.length){
+						s = strings[i];
+					}
+				}
+				for(int j = 0; j < s.length(); j ++){
+					g.drawString(s.charAt(j) + "", j * 8, fm.getAscent() - 1 + i * 12);
+				}
+			}
+		}
+	}
+	private class KeyListener extends KeyAdapter{
+		public void keyTyped(KeyEvent e){
+			try {
+				ssh.output.write(e.getKeyChar());
+				ssh.output.flush();
+			}
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	private class WindowListener extends WindowAdapter{
+		public void windowClosing(WindowEvent e){
+			ssh.channel.disconnect();
+			ssh.session.disconnect();
+		}
 	}
 }
